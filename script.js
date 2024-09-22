46db4c6b76b04aaffff8a812661c7563d07960e1
Kris <you@example.com>
2024-09-22 23:21:20 +0500

    фиксы

--- script.js
+++ script.js
@@ -1,2 +1,85 @@
-const age = 13
-const name = 'Кристи'
+const greeting = 'Привет'
+const goodbye = 'Пока'
+
+const message = `${goodbye}, ${greeting}!`
+console.log (message)
+
+const a = 5
+const b = 20
+const sum = `Сумма a и b равна ${a + b}`
+console.log (sum)
+
+const mess = `
+это
+длинное
+сообщение
+на несколько 
+строк
+`
+console.log (mess)
+
+const age = 19
+const isChild = age < 18
+console.log (isChild)
+
+let data = null
+console.log (data)
+
+let colour
+console.log (colour)
+
+const user = {
+number: 79128160661,
+namе: 'Крис',
+height: 178
+}
+
+console.log (user)
+
+console.log (
+    typeof (111+111)
+)
+
+const num = 111
+const pis = '228'
+const result = num + pis
+console.log (result)
+console.log (typeof result)
+
+console.log ('16' / '8')
+console.log (typeof ('16' / '8'))
+
+const ag = 11
+console.log (ag)
+console.log (String(ag))
+
+const isAdult = false
+console.log (isAdult)
+console.log (
+    String (isAdult)
+)
+
+const obj = {}
+console.log (
+    String (obj)
+)
+
+const str ='222'
+const numb = Number (str)
+console.log (numb)
+console.log (typeof numb)
+
+console.log (
+    Number (null)
+)
+console.log (
+    Number (undefined)
+)
+
+console.log (Boolean (-1))
+console.log (Boolean (0))
+console.log (Boolean (1))
+console.log (Boolean (145))
+console.log (Boolean (''))
+console.log (Boolean (' '))
+console.log (Boolean (undefined))
